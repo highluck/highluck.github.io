a37504fc881725d8988527a1bae04816d09d1e83
highluck <yello1109@daum.net>
2016-08-29 19:37:16 +0900

    width 수정

--- js/app.js
+++ js/app.js
@@ -5,9 +5,10 @@
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: GenericViewCtrl});
+    $routeProvider.when('/startup', {templateUrl: 'partials/startUpPage.html', controller: GenericViewCtrl});
     $routeProvider.when('/project_a', {templateUrl: 'partials/project_a.html', controller: GenericViewCtrl});
     $routeProvider.when('/project_b', {templateUrl: 'partials/project_b.html', controller: GenericViewCtrl});
-    $routeProvider.when('/project_c', {templateUrl: 'partials/project_b.html',controller: GenericViewCtrl});
+    $routeProvider.when('/project_c', {templateUrl: 'partials/project_c.html',controller: GenericViewCtrl});
     $routeProvider.when('/contact', {templateUrl: 'partials/contact.html', controller: ContactViewCtrl});
     $routeProvider.when('/imprint', {templateUrl: 'partials/imprint.html', controller: GenericViewCtrl});
     $routeProvider.otherwise({redirectTo: '/home'});
