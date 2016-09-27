25b8299c2ff318b9b9a9f28d8ab0a19f66e3730a
highluck <yello1109@daum.net>
2016-09-27 16:06:35 +0900

    에러 수정

--- js/directives.js
+++ js/directives.js
@@ -5,7 +5,5 @@
 
 angular.module('myApp.directives', []).
   directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
+    
   }]);
