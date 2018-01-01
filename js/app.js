3288e7d21df20899e98fbdb52fd51b77f6c514d9
high <high.lee@kakaocorp.com>
2018-01-01 18:58:59 +0900

    수정 수정 이름변경

--- js/app.js
+++ js/app.js
@@ -5,130 +5,135 @@
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/home', {
-        templateUrl: 'partials/home.html', 
+        templateUrl: 'partials/home.html',
         controller: GenericViewCtrl
     });
-      
+
     $routeProvider.when('/startup', {
-        templateUrl: 'partials/startUpPage.html', 
+        templateUrl: 'partials/startUpPage.html',
         controller: GenericViewCtrl
     });
-      
+
     $routeProvider.when('/startup/2/', {
-        templateUrl: 'partials/startUpPage_2.html', 
+        templateUrl: 'partials/startUpPage_2.html',
         controller: GenericViewCtrl
     });
-    
+
     $routeProvider.when('/startup/3/', {
-        templateUrl: 'partials/startUpPage_3.html', 
+        templateUrl: 'partials/startUpPage_3.html',
         controller: GenericViewCtrl
     });
-    
+
     $routeProvider.when('/project_a', {
-        templateUrl: 'partials/startup-board/project_a.html', 
+        templateUrl: 'partials/dev-diary-board/project_a.html',
         controller: GenericViewCtrl
     });
-      
+
     $routeProvider.when('/project_b', {
-        templateUrl: 'partials/startup-board/project_b.html', 
+        templateUrl: 'partials/dev-diary-board/project_b.html',
         controller: GenericViewCtrl
     });
-      
+
     $routeProvider.when('/project_c', {
-        templateUrl: 'partials/startup-board/project_c.html',
+        templateUrl: 'partials/dev-diary-board/project_c.html',
         controller: GenericViewCtrl
     });
-      
+
     $routeProvider.when('/project_d', {
-        templateUrl: 'partials/startup-board/project_d.html',
+        templateUrl: 'partials/dev-diary-board/project_d.html',
         controller: GenericViewCtrl
     });
-    
+
     $routeProvider.when('/project_e', {
-        templateUrl: 'partials/startup-board/project_e.html',
+        templateUrl: 'partials/dev-diary-board/project_e.html',
         controller: GenericViewCtrl
     });
-      
+
     $routeProvider.when('/project_f', {
-        templateUrl: 'partials/startup-board/project_f.html',
+        templateUrl: 'partials/dev-diary-board/project_f.html',
         controller: GenericViewCtrl
     });
-      
+
     $routeProvider.when('/project_g', {
-        templateUrl: 'partials/startup-board/project_g.html',
+        templateUrl: 'partials/dev-diary-board/project_g.html',
         controller: GenericViewCtrl
     });
-      
+
     $routeProvider.when('/project_h', {
-        templateUrl: 'partials/startup-board/project_h.html',
+        templateUrl: 'partials/dev-diary-board/project_h.html',
         controller: GenericViewCtrl
     });
-      
+
     $routeProvider.when('/project_i', {
-        templateUrl: 'partials/startup-board/project_i.html',
+        templateUrl: 'partials/dev-diary-board/project_i.html',
         controller: GenericViewCtrl
     });
-      
+
     $routeProvider.when('/project_j', {
-        templateUrl: 'partials/startup-board/project_j.html',
+        templateUrl: 'partials/dev-diary-board/project_j.html',
         controller: GenericViewCtrl
     });
-      
-          
+
+
     $routeProvider.when('/project_k', {
-        templateUrl: 'partials/startup-board/project_k.html',
+        templateUrl: 'partials/dev-diary-board/project_k.html',
         controller: GenericViewCtrl
     });
-      
+
     $routeProvider.when('/project_l', {
-        templateUrl: 'partials/startup-board/project_l.html',
+        templateUrl: 'partials/dev-diary-board/project_l.html',
         controller: GenericViewCtrl
     });
-      
+
     $routeProvider.when('/project_m', {
-        templateUrl: 'partials/startup-board/project_m.html',
+        templateUrl: 'partials/dev-diary-board/project_m.html',
         controller: GenericViewCtrl
     });
-      
+
     $routeProvider.when('/project_o', {
-        templateUrl: 'partials/startup-board/project_o.html',
+        templateUrl: 'partials/dev-diary-board/project_o.html',
         controller: GenericViewCtrl
     });
-      
+
     $routeProvider.when('/project_p', {
-        templateUrl: 'partials/startup-board/project_p.html',
+        templateUrl: 'partials/dev-diary-board/project_p.html',
         controller: GenericViewCtrl
     });
-      
+
+    $routeProvider.when('/project_q', {
+        templateUrl: 'partials/dev-diary-board/project_q.html',
+        controller: GenericViewCtrl
+    });
+
     $routeProvider.when('/troubleshooting', {
-        templateUrl: 'partials/troubleShooting.html', 
+        templateUrl: 'partials/troubleShooting.html',
         controller: GenericViewCtrl
     });
-      
+
     $routeProvider.when('/trouble_a', {
-        templateUrl: 'partials/trouble-board/trouble_a.html', 
+        templateUrl: 'partials/trouble-board/trouble_a.html',
         controller: GenericViewCtrl
     });
-      
+
     $routeProvider.when('/trouble_b', {
-        templateUrl: 'partials/trouble-board/trouble_b.html', 
+        templateUrl: 'partials/trouble-board/trouble_b.html',
         controller: GenericViewCtrl
     });
-      
+
     $routeProvider.when('/trouble_c', {
-        templateUrl: 'partials/trouble-board/trouble_c.html', 
+        templateUrl: 'partials/trouble-board/trouble_c.html',
         controller: GenericViewCtrl
     });
-      
+
     $routeProvider.when('/trouble_d', {
-        templateUrl: 'partials/trouble-board/trouble_d.html', 
+        templateUrl: 'partials/trouble-board/trouble_d.html',
         controller: GenericViewCtrl
     });
-      
+
     $routeProvider.when('/imprint', {
-        templateUrl: 'partials/imprint.html', 
+        templateUrl: 'partials/imprint.html',
         controller: GenericViewCtrl
     });
-      
+
     $routeProvider.otherwise({redirectTo: '/home'});
   }]);
